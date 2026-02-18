7f2e90a55559c5f0bc493b1ee86735648cd5a108
al3xf3r <alessioferraro1993@gmail.com>
2026-02-18 13:48:11 +0100

    lag fix11

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -2167,10 +2167,11 @@ const estNextNano =
 
         <span className="text-sm font-semibold text-orange-300 tabular-nums text-right whitespace-nowrap">
           {fmtCredits8FromNano(v2MissingNano)}{" "}
-          <span className="text-zinc-500 text-xs font-semibold">{husdSymbol}</span>
-        <span className="text-zinc-500"> • </span>
+         
+        
         <span className="text-zinc-500 text-xs font-semibold">Missing</span>{" "}
-          
+          <span className="text-zinc-500"> • </span>
+           <span className="text-zinc-500 text-xs font-semibold">{husdSymbol}</span>
         </span>
       </div>
     </div>
